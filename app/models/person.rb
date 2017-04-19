6b4deb044d69ba1246db0d3a44b086f395a09239
Shahad yousif <shahad_yousif38@yahoo.com>
2017-04-19 12:47:37 +0000

    first commit

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,3 @@
 class Person < ActiveRecord::Base
+   validates:first_name, :last_name, :email, :date_of_birth,  :presence=>true  
 end
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,2 +1,3 @@
 class Vehicle < ActiveRecord::Base
+  validates:vin, :year, :make, :model, :engine_type, :license_plate, :presence=>true  
 end
